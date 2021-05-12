4ea7b754fb9746d1d27a918bbaf96129840d8ed7
James Figures <j@figures.email>
2021-05-12 20:46:10 +1000

    more mess, but still works

--- 2018/day7/day7.cpp
+++ 2018/day7/day7.cpp
@@ -57,6 +57,13 @@ typedef struct steps
 	bool d_met;
 } steps;
 
+typedef struct workers
+{
+	bool free;
+	char c_task;
+	int time_left;
+	int id;
+} workers;
 
 class day
 {
@@ -65,13 +72,17 @@ class day
 public:
 	day();
 	string part1();
-	//int part2();
+	int part2(string);
 	void gen_steps();
 	vector<char> find_start_moves();
 	char find_end();
 	vector<char> find_p_moves(char);
 	bool d_met(char, vector<steps>&);
 	void elim_depend(char, vector<steps>&);
+	void remove_c_from_s(char, string&);
+	int find_time_for_c(char);
+	workers* find_worker(vector<workers>&);
+	vector<char> p2_find_moves(string, vector<steps>&);
 };
 
 day::day() {
@@ -316,11 +327,167 @@ string day::part1()
 	return answer;
 }
 
+void day::remove_c_from_s(char c, string &r_input)
+{
+	string::iterator it;
+	it = find(r_input.begin(), r_input.end(), c);
+	if (it != r_input.end())
+		r_input.erase(it);
+}
+
+
+
+/* returns pointer to first free worker */
+workers* day::find_worker(vector<workers> &r_input)
+{
+	workers *ret_a;
+	for (auto &i: r_input)
+	{
+		if (i.free == true)
+		{
+			ret_a = &i;
+		}
+	}
+	return ret_a;
+}
+
+int day::find_time_for_c(char r_input)
+{
+	int base_time = 60;
+	return (base_time + (r_input - 64));
+}
+
+vector<char> day::p2_find_moves(string path, vector<steps> &r_input)
+{
+	vector<char> ret_a;
+	for (const auto &x: path)
+	{
+		for (const auto &i: r_input)
+		{
+			if (i.step == x)
+			{
+				vector<char> tmp_v = i.dependency;
+				if (tmp_v.size() == 0)
+					ret_a.push_back(i.step);
+			}
+		}
+	}
+	return ret_a;
+}
+
+
+int day::part2(string path)
+{
+	//string path = "FHICMRTXYDBOAJNPWQGVZUEKLS";
+	string pathcp = path;
+	int job_count = pathcp.size();
+	vector<steps> tmp_data = day::data;
+
+	// set up workers
+	vector<workers> my_workers;
+	int WORKER_TOTAL = 5;
+	for (int x = 0; x < WORKER_TOTAL; x++)
+	{
+		workers tmp;
+		tmp.free = true;
+		tmp.id = x+1;
+		my_workers.push_back(tmp);
+	}
+	int current_free_workers = WORKER_TOTAL;
+
+	vector<char> possible_moves = {};
+	bool start = true;
+	int timer = 0;
+	// main loop
+	while(1)
+	{
+
+		// move time forward for our workers
+		for (auto &i: my_workers)
+		{
+			if (i.free == false)
+			{
+				i.time_left--;
+				if (i.time_left == 0)
+				{
+					i.free = true;
+					day::elim_depend(i.c_task, tmp_data);
+					
+					job_count--;
+					i.c_task = 0;
+					current_free_workers++;
+				}
+			}
+		}
+
+		// special run on first loop
+		if (start == true)
+		{
+			start = false;
+			possible_moves = day::find_start_moves();
+
+			for (const auto &i: possible_moves)
+				day::remove_c_from_s(i, pathcp);
+				
+		} else {
+
+			// work out possible moves after 1st loop
+			// find moves who's depencys are met
+			vector<char> tmp_v = day::p2_find_moves(pathcp, tmp_data);
+			if (tmp_v.size() > 0)
+			{
+				for (const auto &i: tmp_v)
+				{
+					day::remove_c_from_s(i, pathcp);
+					possible_moves.push_back(i);
+				}
+			}
+		}
+
+		vector<char> r_from_pm = {};
+		if (possible_moves.size() > 0)
+		{
+			for (int a = 0; ((a < possible_moves.size()) && (current_free_workers > 0)); a++)
+			{
+				//allocate a worker to task
+				char c_tmp = possible_moves.at(a);
+				current_free_workers--;
+
+				workers *found_worker;
+				found_worker = day::find_worker(my_workers);
+
+				found_worker->free = false;
+				found_worker->c_task = c_tmp;
+				found_worker->time_left = day::find_time_for_c(c_tmp);
+				r_from_pm.push_back(c_tmp);
+			}
+
+			// removes jobs applied to workers from possible_moves
+			vector<char>::iterator vc_it;
+			for (const auto &q: r_from_pm)
+			{
+				vc_it = find(possible_moves.begin(), possible_moves.end(), q);
+				if (vc_it != possible_moves.end())
+					possible_moves.erase(vc_it);
+			}
+		}
+
+		if (job_count == 0)
+			break;
+
+		timer++;
+	}
+
+	return timer;
+}
+
 
 int main()
 {
 	day seven;
-	cout << seven.part1() << endl;
+	string p1_answer = seven.part1();
+	cout << p1_answer << endl;
+	cout << seven.part2(p1_answer) << endl;
 
 	return 0;
 }
